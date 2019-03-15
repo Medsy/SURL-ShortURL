f49dda60c850ff1f4e826cbe1fbb5c1fb9babe32
Medsy <rcpkarrinton@hotmail.co.uk>
2019-03-15 16:07:53 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 $browser=false;
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'surl');
-define('BASE_URL', 'http://localhost/WebURLShortener/');
+define('DB_NAME', '');
+define('BASE_URL', '');
 
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
